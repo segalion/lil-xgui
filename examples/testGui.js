06312ce6b7e50ff1f46614787cdcc51b7e05c439
segalion <segalion@gmail.com>
2025-02-21 12:39:20 +0100

    Update testGui.js

--- examples/testGui.js
+++ examples/testGui.js
@@ -1,6 +1,6 @@
 //import {GUI} from "lil-gui";
 // import XGUI from "../src/XGUI";
-import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm';
+import { GUI } from "https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm";
 import { XGui } from "../dist/lil-xgui.min.js";
 /*
 import styles from "../src/styles.css?inline";
